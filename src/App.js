e5ce0b68154a71bdc67c7e2355319ef0d2084b16
Andrea Ruggieri <ruggieri6891@gmail.com>
2023-12-20 09:35:08 +0000

    dinamically create fields

--- src/App.js
+++ src/App.js
@@ -2,27 +2,31 @@ import React from 'react';
 import './App.css';
 
 const fields = [
-    { name: "field_name", label: "Name", type: "text" },
-    { name: "field_username", label: "User", type: "text" },
-    { name: "field_date", label: "Date", type: "date" }
+	{ name: "field_name", label: "Name", type: "text" },
+	{ name: "field_username", label: "User", type: "text" },
+	{ name: "field_date", label: "Date", type: "date" }
 ];
 
 class App extends React.Component {
 
-    render(){
+	render() {
 
-        return (
-        
-<div>
+		return (
 
-    <h1>Webtrends Optimize Software Engineer exam</h1>
-    <form></form>
-</div>
-            
-        );
+			<div>
+				<h1>Webtrends Optimize Software Engineer exam</h1>
+				<form>
+					{fields.map(field => (
+						<>
+							<label>{field.label}</label>
+							<input name={field.name} type={field.type}></input>
+						</>
+					))}
+				</form>
+			</div>
 
-    }
-
+		);
+	}
 }
 
 export default App;
