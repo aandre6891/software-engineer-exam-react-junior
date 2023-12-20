37d9d06fd46a0701523c0bfb02c5bb6cfb89f3dc
Andrea Ruggieri <ruggieri6891@gmail.com>
2023-12-20 10:44:28 +0000

    add test for labels

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,25 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const headingElement = screen.getByText(/Webtrends Optimize Software Engineer exam/i);
-  expect(headingElement).toBeInTheDocument();
+describe('App', () => {
+  const fields = [
+    { name: "field_name", label: "Name", type: "text" },
+    { name: "field_username", label: "User", type: "text" },
+    { name: "field_date", label: "Date", type: "date" }
+  ];
+
+  test('renders learn react link', () => {
+    render(<App />);
+    const headingElement = screen.getByText(/Webtrends Optimize Software Engineer exam/i);
+    expect(headingElement).toBeInTheDocument();
+  });
+
+  test('renders form with labels for each field', () => {
+    render(<App />);
+    fields.forEach((field) => {
+      const labelElement = screen.getByText(field.label);
+      expect(labelElement).toBeInTheDocument();
+    });
+  });
+
 });
