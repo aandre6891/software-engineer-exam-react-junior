9bcc4d4d52aa7d3a25450cc94f6cb68a40185d77
Andrea Ruggieri <ruggieri6891@gmail.com>
2023-12-20 10:14:55 +0000

    add key to dinamic div

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './App.css';
+import New from './components/New';
 
 const fields = [
 	{ name: "field_name", label: "Name", type: "text" },
@@ -17,12 +18,13 @@ class App extends React.Component {
 				<h1>Webtrends Optimize Software Engineer exam</h1>
 				<form>
 					{fields.map(field => (
-						<>
+						<div key={field.name}>
 							<label htmlFor={field.name}>{field.label}</label>
 							<input id={field.name} type={field.type}></input>
-						</>
+						</div>
 					))}
 				</form>
+				<New />
 			</div>
 
 		);
